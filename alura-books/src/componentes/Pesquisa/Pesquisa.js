8094c920baa344b333833e5a88470e2eaf80f245
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-21 16:18:29 -0300

    Estados / 02 - Como funcionam estados

--- alura-books/src/componentes/Pesquisa/Pesquisa.js
+++ alura-books/src/componentes/Pesquisa/Pesquisa.js
@@ -1,39 +1,42 @@
-import styled from "styled-components"
-import Input from "../Input/Input"
+import styled from "styled-components";
+import Input from "../Input/Input";
+import {useState} from 'react'
 
-const PesquisaContainer = styled.section `
-      background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
-        color: #FFF;
-        text-align: center;
-        padding: 85px 0;
-        height: 270px;
-        width: 100%;
-`
+const PesquisaContainer = styled.section`
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+  color: #fff;
+  text-align: center;
+  padding: 85px 0;
+  height: 270px;
+  width: 100%;
+`;
 
 const Titulo = styled.h2`
-        color: #FFF;
-        font-size: 36px;
-        text-align: center;
-        width: 100%;
-`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+`;
 
 const Subtitulo = styled.h3`
-        font-size: 16px;
-        font-weight: 500;
-        margin-bottom: 40px;
-
-`
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 40px;
+`;
 
 const Pesquisa = () => {
-    return (
-        <PesquisaContainer>
-            <Titulo> Já sabe por onde começar? </Titulo>
-            <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
-            <Input placeholder="Escreva sua próxima leitura" />
-        </PesquisaContainer>
-    )
-}
+    const [textoDigitado, setTextoDigitado] = useState(" ")
 
+  return (
+    <PesquisaContainer>
+      <Titulo> Já sabe por onde começar? </Titulo>
+      <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
+      <Input placeholder="Escreva sua próxima leitura"
+             onBlur={e =>  setTextoDigitado(e.target.value)} 
+       />
+       <p> {textoDigitado} </p>
+    </PesquisaContainer>
+  );
+};
 
-export default Pesquisa
-
+export default Pesquisa;
