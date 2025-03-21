fcfde03e9e2afeaa7ba27da7444c7d88127c7a12
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-21 09:42:04 -0300

    StyledComponents / 01 - Criando containers

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+
 import { Header } from './componentes/Header/Header';
+import styled from 'styled-components'
+
+//compoenentizando o css
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002f52 35%, #326589 165%)
+`
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header/>
-      
-    </div>
+
+    </AppContainer>
   );
 }
 
--- alura-books/src/componentes/Header/Header.js
+++ alura-books/src/componentes/Header/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Logo from '../Logo/Logo'
 import NavBar from '../NAV/NavBar'
-import styles from './Header.module.css'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+
+
+`
 
 export const Header = () => {
   return (
-    <header className={styles.header}>
+    <HeaderContainer>
         <Logo/>
         <NavBar/>
-      </header>
+      </HeaderContainer>
   )
 }
