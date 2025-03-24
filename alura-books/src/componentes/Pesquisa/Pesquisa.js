0b7b6eebed0782faea0e001ff1d996224727e932
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-24 17:18:42 -0300

    Estados / 03 - Atualizando estados com onBlur 2/2

--- alura-books/src/componentes/Pesquisa/Pesquisa.js
+++ alura-books/src/componentes/Pesquisa/Pesquisa.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Input from "../Input/Input";
 import {useState} from 'react'
+import { livros } from "./DadosPesquisas";
+
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -25,16 +27,22 @@ const Subtitulo = styled.h3`
 `;
 
 const Pesquisa = () => {
-    const [textoDigitado, setTextoDigitado] = useState(" ")
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+
+    console.log(livrosPesquisados)
 
   return (
     <PesquisaContainer>
       <Titulo> Já sabe por onde começar? </Titulo>
       <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
       <Input placeholder="Escreva sua próxima leitura"
-             onChanged={e =>  setTextoDigitado(e.target.value)} 
+             onChange={e =>  {
+                const textoDigitado = e.target.value
+                const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
+                setLivrosPesquisados(resultadoPesquisa)
+             }} 
        />
-       <p> {textoDigitado} </p>
+      
     </PesquisaContainer>
   );
 };
