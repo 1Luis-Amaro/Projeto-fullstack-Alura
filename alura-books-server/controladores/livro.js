dc57085fd588de1fdbe02cf5335860f193ca6f9f
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-03-27 17:51:22 -0300

    Parâmetros na requisições / 01 - Lidando com Parametros

--- alura-books-server/controladores/livro.js
+++ alura-books-server/controladores/livro.js
@@ -1,11 +1,25 @@
-const { getTodosLivros } = require("../servicos/livro")
+const { getTodosLivros, getLivroPorId } = require("../servicos/livro")
 
 fs = require("fs")
 
 const getLivros = (req, res)  => {
         try {
-            const livros = getTodosLivros
+            const livros = getTodosLivros()
             res.send(livros)
+        } catch (error) {
+            res.status(500)
+            res.send(error.message)
+        }
+    
+
+    }
+
+    const getLivro = (req, res)  => {
+        
+        try {
+            const id = req.params.id
+            const livro = getLivroPorId(id)
+            res.send(livro)
         } catch (error) {
             res.status(500)
             res.send(error.message)
@@ -15,5 +29,6 @@ const getLivros = (req, res)  => {
 
 
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro
 }
--- alura-books-server/servicos/livro.js
+++ alura-books-server/servicos/livro.js
@@ -4,7 +4,15 @@ const getTodosLivros = () => {
 
 }
 
+const getLivroPorId = (id) => {
+    const livros = JSON.parse(fs.readFileSync("livros.json"))
+
+    const livroFiltrado = livros.filter(livro => livro.id === id ) [0]
+    return livroFiltrado
+}
+
 
 module.exports = {
-    getTodosLivros
+    getTodosLivros,
+    getLivroPorId
 }
